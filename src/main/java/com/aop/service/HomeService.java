9a7ccbcfaa7d5b5700d69648c355ff951b1d6757
Alexander Lisay <alexanderli@softwareplanet.uk.com>
2015-02-05 20:53:52 +0200

    - updated Procfile

---
+++ src/main/java/com/aop/service/HomeService.java
@@ -0,0 +1,7 @@
+package com.aop.service;
+
+/**
+ * Created by alexanderli on 2/5/15.
+ */
+public class HomeService {
+}
---
+++ src/main/java/com/aop/util/DataViewBean.java
@@ -0,0 +1,7 @@
+package com.aop.util;
+
+/**
+ * Created by alexanderli on 2/5/15.
+ */
+public class DataViewBean {
+}
